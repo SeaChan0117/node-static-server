52d1377d1df2f49d1e8d4a8c70896b48c57a988b
SeaChan <1341914555@qq.com>
2021-10-15 14:06:32 +0800

    feat: md 文档解析展示

--- main.js
+++ main.js
@@ -6,6 +6,25 @@ const {createReadStream} = require('fs')
 const mime = require('mime')
 const ejs = require('ejs')
 const {promisify} = require('util')
+const markdownItAnchor = require('markdown-it-anchor')
+const markdownItTocDoneRight = require('markdown-it-toc-done-right')
+
+const mdOption = {
+    html: false,
+    xhtmlOut: true,
+    typographer: true
+}
+
+const markdownItAnchorOption = {permalink: true, permalinkBefore: true, permalinkSymbol: '§'}
+const markdownItTocDoneRightOption = {
+    containerClass: 'toc',
+    containerId: 'toc',
+    listType: 'ul',
+    listClass: 'listClass',
+    itemClass: 'itemClass',
+    linkClass: 'linkClass'
+}
+const md = require("markdown-it")(mdOption)
 
 function mergeConfig(config) {
     return {
@@ -31,12 +50,11 @@ class Server {
         let {pathname} = url.parse(req.url)
         pathname = decodeURIComponent(pathname)
         let absPath = path.join(this.config.directory, pathname)
-
         try {
             let statObj = await fs.stat(absPath)
             if (statObj.isFile()) {
                 // 文件
-                this.fileHandle(req, res, absPath)
+                await this.fileHandle(req, res, absPath)
             } else {
                 // 文件夹
                 let dirs = await fs.readdir(absPath)
@@ -47,6 +65,9 @@ class Server {
                 }))
                 const renderFile = promisify(ejs.renderFile)
                 const parentPath = path.dirname(pathname)
+
+                this.beforePath = pathname
+
                 const ret = await renderFile(path.resolve(__dirname, 'template.html'), {
                     arr: dirs,
                     parent: pathname !== '/',
@@ -61,10 +82,41 @@ class Server {
         }
     }
 
-    fileHandle(req, res, absPath) {
+    async fileHandle(req, res, absPath) {
         res.statusCode = 200
-        res.setHeader('Content-Type', `${mime.getType(absPath)}; charset=utf-8`)
-        createReadStream(absPath).pipe(res)
+        if (absPath.endsWith('.md')) {
+            await this.readMdFile(req, res, absPath)
+        } else {
+            res.setHeader('Content-Type', `${mime.getType(absPath)}; charset=utf-8`)
+            createReadStream(absPath).pipe(res)
+        }
+    }
+
+    async readMdFile (req, res, absPath) {
+        const data = await fs.readFile(absPath)
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        markdownItTocDoneRightOption.callback = (html, ast) => {
+            // 缓存 md 解析出的左侧导航栏
+            this.mdMenu = html
+        }
+        // 保证只加载一次插件
+        if (!this.mdPlugins) {
+            md.use(markdownItAnchor, markdownItAnchorOption)
+                .use(markdownItTocDoneRight, markdownItTocDoneRightOption)
+            this.mdPlugins = true
+        }
+        const htmlForMd = md.render(data.toString('utf-8'))
+        const styles = await fs.readFile(path.join(__dirname, '/assets/github-markdown.css'))
+        const renderFile = promisify(ejs.renderFile)
+        console.log('---', this.beforePath)
+        const ret = await renderFile(path.resolve(__dirname, 'md-render.html'), {
+            mdMenu: this.mdMenu,
+            mdData: htmlForMd,
+            beforePath: this.beforePath || '',
+            stylesStr: styles.toString('utf-8')
+        })
+
+        res.end(ret)
     }
 
     errHandle(req, res, e) {
